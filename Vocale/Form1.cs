d3c54148b953dda3d1a7e546c9af19dd4a8f026d
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2025-02-05 19:39:23 +0200

    vocale

---
+++ Vocale/Form1.cs
@@ -0,0 +1,27 @@
+﻿namespace Vocale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string text = textBox1.Text.ToLower(); 
+
+            int vocalCount = 0;
+
+            foreach (char c in text)
+            {
+                if ("aeiouy".Contains(c))
+                {
+                    vocalCount++;
+                }
+            }
+
+            this.Text = $"Număr de vocale: {vocalCount}";
+        }
+    }
+}
