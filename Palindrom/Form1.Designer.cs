c4161bd6b0f348a0a029c183f778f935c25319dc
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2025-02-05 20:20:43 +0200

    palindrom

---
+++ Palindrom/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Palindrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkButton = new Button();
+            textBox1 = new TextBox();
+            resultLabel = new Label();
+            SuspendLayout();
+            // 
+            // checkButton
+            // 
+            checkButton.Location = new Point(676, 59);
+            checkButton.Name = "checkButton";
+            checkButton.Size = new Size(112, 34);
+            checkButton.TabIndex = 0;
+            checkButton.Text = "verifica";
+            checkButton.UseVisualStyleBackColor = true;
+            checkButton.Click += checkButton_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(43, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(627, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // resultLabel
+            // 
+            resultLabel.AutoSize = true;
+            resultLabel.Location = new Point(43, 265);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new Size(0, 25);
+            resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(resultLabel);
+            Controls.Add(textBox1);
+            Controls.Add(checkButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button checkButton;
+        private TextBox textBox1;
+        private Label resultLabel;
+    }
+}
---
+++ Palindrom/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace Palindrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            string cuvant = textBox1.Text;
+
+            if (string.IsNullOrEmpty(cuvant))
+            {
+                resultLabel.Text = "Vă rugăm să introduceți un cuvânt!";
+                return;
+            }
+
+            string cuvantInversat = ReverseString(cuvant);
+
+            if (cuvant.Equals(cuvantInversat, StringComparison.OrdinalIgnoreCase))
+            {
+                resultLabel.Text = "Cuvântul este palindrom!";
+            }
+            else
+            {
+                resultLabel.Text = "Cuvântul nu este palindrom.";
+            }
+        }
+
+        private string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
+
