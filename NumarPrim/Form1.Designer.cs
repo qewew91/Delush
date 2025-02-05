80dc54acc90141c379ebc33b0d15955e5f2fd919
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2025-02-05 21:27:36 +0200

    numar prim

---
+++ NumarPrim/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace NumarPrim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            resultLabel = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(676, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "determina";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(658, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // resultLabel
+            // 
+            resultLabel.AutoSize = true;
+            resultLabel.Location = new Point(12, 83);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new Size(0, 25);
+            resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(resultLabel);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label resultLabel;
+    }
+}
---
+++ NumarPrim/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace NumarPrim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsPrime(int numar)
+        {
+            if (numar <= 1) return false; 
+            for (int i = 2; i <= Math.Sqrt(numar); i++) 
+            {
+                if (numar % i == 0) return false; 
+            }
+            return true; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numar = int.Parse(textBox1.Text);
+
+                StringBuilder primeNumbers = new StringBuilder();
+
+                for (int i = 2; i <= numar; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        primeNumbers.Append(i + " "); 
+                    }
+                }
+
+                resultLabel.Text = primeNumbers.Length > 0 ? primeNumbers.ToString() : "Nu există numere prime în acest interval.";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Introduceți un număr valid!");
+            }
+        }
+    }
+}
