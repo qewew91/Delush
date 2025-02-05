fe45455209b107218254853af967a8fe83c304a1
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2025-02-05 20:42:53 +0200

    Litere

---
+++ Litere/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Litere
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            excludeButton = new Button();
+            arrayTextBox = new TextBox();
+            resultLabel = new Label();
+            SuspendLayout();
+            // 
+            // excludeButton
+            // 
+            excludeButton.Location = new Point(676, 12);
+            excludeButton.Name = "excludeButton";
+            excludeButton.Size = new Size(112, 34);
+            excludeButton.TabIndex = 0;
+            excludeButton.Text = "exclude";
+            excludeButton.UseVisualStyleBackColor = true;
+            excludeButton.Click += excludeButton_Click;
+            // 
+            // arrayTextBox
+            // 
+            arrayTextBox.Location = new Point(22, 12);
+            arrayTextBox.Name = "arrayTextBox";
+            arrayTextBox.Size = new Size(648, 31);
+            arrayTextBox.TabIndex = 1;
+            // 
+            // resultLabel
+            // 
+            resultLabel.AutoSize = true;
+            resultLabel.Location = new Point(22, 69);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new Size(0, 25);
+            resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(resultLabel);
+            Controls.Add(arrayTextBox);
+            Controls.Add(excludeButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button excludeButton;
+        private TextBox arrayTextBox;
+        private Label resultLabel;
+    }
+}
---
+++ Litere/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Litere
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void excludeButton_Click(object sender, EventArgs e)
+        {
+            string initialText = arrayTextBox.Text;
+
+            if (string.IsNullOrEmpty(initialText))
+            {
+                resultLabel.Text = "Introduceți un șir de caractere!";
+                return;
+            }
+
+            StringBuilder textWithoutDigits = new StringBuilder();
+
+            foreach (char c in initialText)
+            {
+                if (!Char.IsDigit(c))  
+                {
+                    textWithoutDigits.Append(c);  
+                }
+            }
+
+            resultLabel.Text = "Șirul fără cifre este: " + textWithoutDigits.ToString();
+        }
+    }
+}
