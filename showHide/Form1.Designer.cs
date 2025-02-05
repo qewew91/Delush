b558256e2acb78fc48688f93fafc53852c6db839
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2025-02-05 17:03:18 +0200

    Add project files.

---
+++ showHide/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace showHide
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            show = new Button();
+            hide = new Button();
+            showHide = new Button();
+            SuspendLayout();
+            // 
+            // show
+            // 
+            show.Location = new Point(81, 517);
+            show.Name = "show";
+            show.Size = new Size(112, 34);
+            show.TabIndex = 0;
+            show.Text = "Show";
+            show.UseVisualStyleBackColor = true;
+            show.Click += button1_Click;
+            // 
+            // hide
+            // 
+            hide.Location = new Point(330, 517);
+            hide.Name = "hide";
+            hide.Size = new Size(112, 34);
+            hide.TabIndex = 1;
+            hide.Text = "Hide";
+            hide.UseVisualStyleBackColor = true;
+            hide.Click += hide_Click;
+            // 
+            // showHide
+            // 
+            showHide.Location = new Point(765, 517);
+            showHide.Name = "showHide";
+            showHide.Size = new Size(112, 34);
+            showHide.TabIndex = 2;
+            showHide.Text = "Show/Hide";
+            showHide.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1089, 621);
+            Controls.Add(showHide);
+            Controls.Add(hide);
+            Controls.Add(show);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button show;
+        private Button hide;
+        private Button showHide;
+    }
+}
---
+++ showHide/Form1.cs
@@ -0,0 +1,20 @@
+namespace showHide
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.showHide.Show();
+        }
+
+        private void hide_Click(object sender, EventArgs e)
+        {
+            this.showHide.Hide();
+        }
+    }
+}
