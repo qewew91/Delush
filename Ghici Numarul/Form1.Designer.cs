b12ef008271ee1e1b0e76e42422e6348d6862f43
Dobrovolschi Silviu <silviu.dobrik@gmail.com>
2025-02-05 21:03:20 +0200

    ghici numarul

---
+++ Ghici Numarul/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Ghici_Numarul
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            guessButton = new Button();
+            resultLabel = new Label();
+            guessedNumberTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // guessButton
+            // 
+            guessButton.Location = new Point(676, 12);
+            guessButton.Name = "guessButton";
+            guessButton.Size = new Size(112, 34);
+            guessButton.TabIndex = 0;
+            guessButton.Text = "Ghici";
+            guessButton.UseVisualStyleBackColor = true;
+            guessButton.Click += guessButton_Click;
+            // 
+            // resultLabel
+            // 
+            resultLabel.AutoSize = true;
+            resultLabel.Location = new Point(12, 108);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new Size(0, 25);
+            resultLabel.TabIndex = 1;
+            // 
+            // guessedNumberTextBox
+            // 
+            guessedNumberTextBox.Location = new Point(12, 15);
+            guessedNumberTextBox.Name = "guessedNumberTextBox";
+            guessedNumberTextBox.Size = new Size(658, 31);
+            guessedNumberTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(guessedNumberTextBox);
+            Controls.Add(resultLabel);
+            Controls.Add(guessButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button guessButton;
+        private Label resultLabel;
+        private TextBox guessedNumberTextBox;
+    }
+}
---
+++ Ghici Numarul/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace Ghici_Numarul
+{
+    public partial class Form1 : Form
+    {
+        private int generatedNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateNumber();
+        }
+
+        private void GenerateNumber()
+        {
+            Random random = new Random();
+            generatedNumber = random.Next(0, 10);
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int guessedNumber = int.Parse(guessedNumberTextBox.Text);
+
+                if (guessedNumber == generatedNumber)
+                {
+                    resultLabel.Text = "Felicitări! Ai ghicit numărul!";
+                }
+                else
+                {
+                    resultLabel.Text = "Numărul generat a fost: " + generatedNumber.ToString();
+                }
+
+                GenerateNumber();
+            }
+            catch (FormatException)
+            {
+                resultLabel.Text = "Eroare: Introduceți un număr între 0 și 9!";
+            }
+        }
+    }
+}
